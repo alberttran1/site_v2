b29b417cc81cca8851868fd1bbd746cc01e5deec
Albert Tran <albertq.tran1@gmail.com>
2024-10-17 12:42:07 -0400

    fixed topbar z-index

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -4,7 +4,7 @@ const Topbar = (props : {aboutRef: RefObject<HTMLDivElement>, experienceRef: Ref
   const {aboutRef, experienceRef, projectsRef} = props;
 
   return (
-    <div className="absolute w-full h-14 top-0 border-b border-b-black justify-between gap-2 text-black flex font-Abel bg-[#F9F6F1]">
+    <div className="absolute z-10 w-full h-14 top-0 border-b border-b-black justify-between gap-2 text-black flex font-Abel bg-[#F9F6F1]">
       <div className="flex justify-center items-center gap-4 underline-offset-4">
         <div className="flex h-full w-[54px] items-center justify-center border-r border-r-black bg-black text-[#F9F6F1] cursor-pointer"
           onClick={() => window.location.reload()}>
